ba8451ca8245a0e13cd985e9db50dace4885064a
caydey <caydey@protonmail.com>
2022-03-15 15:10:32 +0000

    initial vuejs template for jack

---
+++ service/client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>App</h1>
+
+    <InputSrcDest />
+    <WikiPath v-bind:path="[{title: 'God', url: 'wikipedia.com/god'}, {title: 'Elliot', url: 'wiki.com/smelliot'}]" />
+
+  </div>
+</template>
+
+<style scoped>
+#app {
+  background: lightblue;
+}
+</style>
+
+<script>
+import WikiPath from './components/WikiPath.vue'
+import InputSrcDest from './components/InputSrcDest.vue'
+export default {
+  name: 'App',
+  components: {
+    WikiPath,
+    InputSrcDest
+  }
+}
+</script>
+
