87421695e0841e6ce9c7f6a92a70559f52e64a18
caydey <caydey@protonmail.com>
2022-03-18 00:02:17 +0000

    replaced redis with sql

--- service/server/api/v1/linkPages.js
+++ service/server/api/v1/linkPages.js
@@ -1,75 +1,88 @@
-const express = require('express');
-const redis = require('redis');
-const { createTitleHash } = require.main.require('./services/titleHash.js');
-const { findShortestPath } = require.main.require('./services/shortestPath.js');
-var router = express.Router();
+const express = require('express')
+const { findShortestPath, isIndexedTitle } = require.main.require('./services/shortestPath.js')
+var router = express.Router()
 
 
 router.post('/', (req,res) => {
-  const src = req.body.src;
-  const dest = req.body.dest;
+  const src = req.body.src
+  const dest = req.body.dest
 
   if (src && dest) {
-    srcTitleUrl = convertWikiTitleUrl(src);
-    destTitleUrl = convertWikiTitleUrl(dest);
-    checkValidTitle(srcTitleUrl.title, (validSrc) => {
-      checkValidTitle(destTitleUrl.title, (validDest) => {
-        if (validSrc && validDest) {
+    srcTitleUrl = createPageObject(src)
+    destTitleUrl = createPageObject(dest)
+    checkValidTitle(srcTitleUrl.title, (validSrc) => { // 'src' valid title
+      checkValidTitle(destTitleUrl.title, (validDest) => { // 'dest' valid title
+        if (validSrc && validDest) { // valid titles
           findShortestPath(srcTitleUrl.title, destTitleUrl.title, (titlePath, queryTime) => {
-            const steps = titlePath.length-1;
-            var path = [];
-            for (let i=0; i<titlePath.length; i++) {
-              path.push(convertWikiTitleUrl(titlePath[i]));
+            if (titlePath.length === 0) {
+              res.status(400).send({
+                success: false,
+                message: 'Search exhausted',
+                data: {
+                  src: src,
+                  dest: dest
+                }
+              })
+            } else {
+              const steps = titlePath.length-1;
+              var path = []
+              for (let i=0; i<titlePath.length; i++) {
+                path.push(createPageObject(titlePath[i]))
+              }
+              res.status(200).send({
+                success: true,
+                queryTime: queryTime,
+                steps: steps,
+                src: srcTitleUrl,
+                dest: destTitleUrl,
+                path: path
+              })
             }
-            res.status(200).send({
-              success: true,
-              queryTime: queryTime,
-              steps: steps,
-              src: srcTitleUrl,
-              dest: destTitleUrl,
-              path: path
-            })
           })
         } else {
           res.status(400).send({
             success: false,
-            message: 'wikipedia title not found',
+            message: 'Wikipedia title not found',
             notFoundField: (!validSrc) ? 'src' : 'dest',
             data: {
               src: src,
               dest: dest
             }
-          });
+          })
         }
-      });
-    });
+      })
+    })
   } else {
     res.status(400).send({
       success: false,
       message: 'Error src or dest not specified'
-    });
+    })
   }
-});
+})
 
-function convertWikiTitleUrl(rawTitle) {
+function createPageObject(rawTitle) {
   // https://en.wikipedia.org/wiki/Linux > Linux
   const s = rawTitle.split(/(https?:\/\/)?(en\.)?wikipedia\.(org|com)\/wiki\//)
   var title = s[s.length-1]
-  title = title.replaceAll('_', ' ') // actual titles not the titles as appearing in url
-  var url = 'https://en.wikipedia.org/wiki/'+title.replaceAll(' ', '_')
+  
+  // title as its stored in the database
+  var dbTitle = title.replaceAll('_', ' ') // actual titles not the titles as appearing in url
+  dbTitle = dbTitle.replaceAll('%20', ' ') // encoded spa
+
+  // title as a url
+  var urlTitle = title.replaceAll(' ','_')
+  var url = 'https://en.wikipedia.org/wiki/'+urlTitle
+
+  // return page object
   return {
-    title: title,
+    title: dbTitle,
     url: url
   }
 }
 
 async function checkValidTitle(title, callback) {
-  var client = redis.createClient();
-  await client.connect();
-  const titleHash = createTitleHash(title);
-  const found = await client.hExists('titles', titleHash);
-  client.disconnect();
-  callback(found);
+  const valid = await isIndexedTitle(title)
+  callback(valid);
 }
 
 exports.router = router;
--- service/server/services/shortestPath.js
+++ service/server/services/shortestPath.js
@@ -1,136 +1,37 @@
-const redis = require('redis');
-const { createTitleHash } = require('./titleHash.js');
-const { pool } = require.main.require('./pool.js');
+const { pool } = require.main.require('./pool.js')
+
+
 
 
 async function findShortestPath(src, dest, callback) {
-  const queryStartTime = Date.now();
-  const client = redis.createClient();
-  await client.connect();
-
-
-  var visited = new Map(); // value, parent
-  var queue = [];
-
-  const search_src_hash = createTitleHash(src);
-  const search_dest_hash = createTitleHash(dest);
-
-  queue.push(search_src_hash)
-  
-  var focus = search_src_hash;
-  while (queue.length > 0) {
-    focus = queue.shift();
-
-    var focusNodes = await client.sMembers(focus);
-    if (focusNodes.includes(search_dest_hash)) {
-      visited.set(search_dest_hash, focus)
-      break;
-    }
-    for (let i=0; i<focusNodes.length; i++) {
-      if (!visited.has(focusNodes[i])) {
-        queue.push(focusNodes[i]);
-        visited.set(focusNodes[i], focus)
-      }
-    }
-  }
-
-  var hashPath = []
-  var lastParent = search_dest_hash
-  // find path
-  hashPath.push(lastParent)
-  while (lastParent != search_src_hash) {
-    lastParent = visited.get(lastParent)
-    hashPath.push(lastParent)
-  }
-
-  var path = []
-  for (let i=hashPath.length-1; i>=0; i--) {
-    const title = await client.hGet("titles", hashPath[i])
-    path.push(title)
-  }
-  await client.disconnect();
-
-  const queryEndTime = Date.now();
-  const queryTime = queryEndTime - queryStartTime;
-
-  // return path
-  callback(path, queryTime)
-}
-exports.findShortestPath = findShortestPath;
-
-
-async function poolQuery(query, data=[]) { // await pool.query dont workie
-  return new Promise((resolve, reject) => {
-    pool.query(query, data, (err, response) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(response);
-      }
-    });
-  });
-}
-
-function get_path(src, dest, src_visited, dest_visited, intersection) {
-  // flip(intersection -> src) -> (intersection -> dest)
-  console.log(src,dest);
-  var path = []
-  path.push(intersection)
-  var focus = intersection
-  while (focus !== src) {
-    focus = src_visited.get(focus)
-    path.push(focus)
-  }
-
-  path.reverse()
-
-  focus = intersection
-  while (focus !== dest) {
-    focus = dest_visited.get(focus)
-    path.push(focus)
-  }
-
-  return path
-}
-
-function check_intersection(src_visited, dest_visited) {
-  // maybe iterate over smallest map
-  for (focus of src_visited.keys()) {
-    if (dest_visited.has(focus)) {
-      return focus
-    }
-  }
-  return undefined
-}
-
-
-async function findShortestPathSql(src, dest, callback) {
-  // const val = await pool.query('SELECT title FROM Page LIMIT 10');
-  // console.log(val);
-  // const val = await poolQuery('SELECT title FROM Page LIMIT 10')
+  //// start query time ////
   const queryStartTime = Date.now()
   
+  // get database ids for titles
   var srcRow = await poolQuery(`SELECT id FROM Page WHERE title=?`, src)
   var destRow = await poolQuery(`SELECT id FROM Page WHERE title=?`, dest)
   const srcId = srcRow[0].id
   const destId = destRow[0].id
 
+  // search queue
   var src_queue = []
   var dest_queue = []
   src_queue.push(srcId)
   dest_queue.push(destId)
 
+  // visited map for backtracking steps to make path
   var src_visited = new Map() // [title, parent]
-  var dest_visited = new Map()
+  var dest_visited = new Map() // [title, parent]
   src_visited.set(srcId, -1)
   dest_visited.set(destId, -1)
 
+  // iterate until intersection found or search exhausted
   var intersection = undefined
   while (intersection == undefined && src_queue.length != 0 && dest_queue.length != 0) {
     // forward bfs and backwards bfs *should* be the exact same but just with a few variable changes
-    // forward bfs
+    //// forward bfs ////
     const src_focus = src_queue.shift() // pop
-    const src_children = await poolQuery(`SELECT dest FROM Link WHERE src=?`, src_focus);
+    const src_children = await poolQuery(`SELECT dest FROM Link WHERE src=?`, src_focus); // get child nodes
     for (let i=0; i<src_children.length; i++) {
       if (!src_visited.has(src_children[i].dest)) { // not already visited
         src_visited.set(src_children[i].dest, src_focus)
@@ -138,9 +39,9 @@ async function findShortestPathSql(src, dest, callback) {
       }
     }
 
-    // backwards bfs
+    //// backwards bfs ////
     const dest_focus = dest_queue.shift() // pop
-    const dest_children = await poolQuery(`SELECT src FROM Link WHERE dest=?`, dest_focus);
+    const dest_children = await poolQuery(`SELECT src FROM Link WHERE dest=?`, dest_focus); // get child nodes
     for (let i=0; i<dest_children.length; i++) {
       if (!dest_visited.has(dest_children[i].src)) { // not already visited
         dest_visited.set(dest_children[i].src, dest_focus)
@@ -148,26 +49,83 @@ async function findShortestPathSql(src, dest, callback) {
       }
     }
 
+    // check for intersection
     intersection = check_intersection(src_visited, dest_visited)
   }
-  
-  const queryEndTime = Date.now();
-  const queryTime = queryEndTime - queryStartTime;
+  var path = [] // return empty path array if no path found
   if (intersection) {
-    const path = get_path(srcId, destId, src_visited, dest_visited, intersection)
-    var titlePath = []
-    for (let i=0; i<path.length; i++) {
-      const title = await poolQuery(`SELECT title FROM Page where id=?`,path[i]);
-      titlePath.push((title[0].title).toString())
-    }
-    callback(titlePath, queryTime)
-  } else {
-    console.log("no path found");
-    callback([],0)
+    // remake path from visited nodes
+    path = await get_path(srcId, destId, src_visited, dest_visited, intersection)
   }
 
+  //// end query time ////
+  const queryEndTime = Date.now()
+  const queryTime = queryEndTime - queryStartTime
 
-
-  // return path
+  // return
+  callback(path, queryTime)
 }
-exports.findShortestPathSql = findShortestPathSql;
+exports.findShortestPath = findShortestPath
+
+async function isIndexedTitle(title) {
+  const row = await poolQuery(`SELECT id FROM Page WHERE title=?`, title)
+  if (row.length === 0) {
+    return false
+  }
+  return true
+}
+exports.isIndexedTitle = isIndexedTitle
+
+// for some strange reason the program will not wait when using 'await pool.query' on a single line
+async function poolQuery(query, data=[]) {
+  return new Promise((resolve, reject) => {
+    pool.query(query, data, (err, response) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(response)
+      }
+    })
+  })
+}
+
+async function get_path(src, dest, src_visited, dest_visited, intersection) {
+  // flip(intersection -> src) -> (intersection -> dest)
+
+  var path = []
+  // get path from intersection point to the start of the search
+  path.push(intersection)
+  var focus = intersection
+  while (focus !== src) {
+    focus = src_visited.get(focus)
+    path.push(focus)
+  }
+  // reverse path so its now from the start of the search to the intersection point
+  path.reverse()
+
+  // append to path the path from the intersection point to the end of the search
+  focus = intersection
+  while (focus !== dest) {
+    focus = dest_visited.get(focus)
+    path.push(focus)
+  }
+
+  // convert the path of node ids to the actual titles of the wikipedia pages
+  var titlePath = []
+  for (let i=0; i<path.length; i++) {
+    const row = await poolQuery(`SELECT title FROM Page where id=?`,path[i])
+    titlePath.push((row[0].title).toString())
+  }
+
+  return titlePath
+}
+
+function check_intersection(src_visited, dest_visited) {
+  for (focus of src_visited.keys()) { // for each key of the src_visited map
+    if (dest_visited.has(focus)) { // check if it exists in the dest_visited map
+      return focus  // return intersection
+    }
+  }
+  // no intersection
+  return undefined
+}
