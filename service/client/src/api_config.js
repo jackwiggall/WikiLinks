f7d6c9b9b374724579eb2d82e4c42db0c6109077
caydey <caydey@protonmail.com>
2022-03-20 16:32:53 +0000

    development vuejs api mode

--- service/client/src/api_config.js
+++ service/client/src/api_config.js
@@ -1,6 +1,9 @@
-const API_URL = 'http://141.98.252.168:55533/'
+var API_HOST = 'http://141.98.252.168:55533/'
+if (process.env.NODE_ENV === 'development') {
+  API_HOST = 'http://localhost:3000/'
+}
 module.exports = {
-  API_URL: API_URL,
-  API_LINK_PAGES_DEV: API_URL+'api/v1/devLinkPages',
-  API_LINK_PAGES: API_URL+'api/v1/linkPages'
+  API_HOST: API_HOST,
+  API_LINK_PAGES_DEV: API_HOST+'api/v1/devLinkPages',
+  API_LINK_PAGES: API_HOST+'api/v1/linkPages'
 }
