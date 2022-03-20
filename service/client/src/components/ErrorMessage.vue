0a2143a9a54a181d73bb23df0cf1e7a88d1cce0d
caydey <caydey@protonmail.com>
2022-03-20 02:47:06 +0000

    error message

---
+++ service/client/src/components/ErrorMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-warning container mt-4 p-3 ">
+    <span>
+      ERROR:
+      {{message}}
+    </span>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+  name: 'ErrorMessage',
+  props: [
+    'message'
+  ]
+}
+</script>
